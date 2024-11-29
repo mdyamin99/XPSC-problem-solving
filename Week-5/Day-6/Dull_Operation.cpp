c5578f77537e70769a4eaae7b20273c8210a25b4
mdyamin99 <you@example.com>
2024-11-29 18:48:38 +0600

    added week-5 day-6 codes

---
+++ Week-5/Day-6/Dull_Operation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define print(v) for(auto data : v) cout<<data<<" "; cout<<endl
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<n<<" "<<n-1<<endl;
+    }
+    return 0;
+}
