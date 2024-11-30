8e4187296a614647878c14caf817cf6ad5f231e8
mdyamin99 <you@example.com>
2024-11-30 19:03:16 +0600

    added week-5 day-7 codes

---
+++ Week-5/Day-7/B_Battle_for_Survive.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define print(v) for(auto data : v) cout<<data<<" "; cout<<endl
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        ll ans=v[n-2];
+        for(int i=0;i<n-2;i++){
+            ans=ans-v[i];
+        }
+        cout<<v[n-1]-ans<<endl;
+    }
+    return 0;
+}
